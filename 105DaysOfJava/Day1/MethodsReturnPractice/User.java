c18f3d42b7c051cc1c4424bdc288bf0366bf8905
Chris <100327446+cwindrow007@users.noreply.github.com>
2024-08-29 16:06:05 -0400

    Create User.java

---
+++ 105DaysOfJava/Day1/MethodsReturnPractice/User.java
@@ -0,0 +1,9 @@
+public class User {
+    public String firstName; //Decalred Public variable which is owned to class
+    public String lastName;
+
+    public String output() { //method(Function) which is output and handles returning first/last name.
+       return "Hi my name is "  + firstName + " " + lastName + "."; //Return Statement
+        }
+    }
+
